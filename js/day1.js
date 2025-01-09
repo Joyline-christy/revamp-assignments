d5eec09ee7dccb0b55a33e5cb943e589d1f4f465
Joyline-christy <joychristy231201@gmail.com>
2025-01-09 13:14:30 +0530

    assignment 9

---
+++ js/day1.js
@@ -0,0 +1,43 @@
+/*let username ='joyline christy';
+let age =23;
+
+console.log(`the user name is: ${username} and age is: ${age}`);
+
+
+let num1=50;
+let num2=30;
+console.log(`the addition of num1  ${num1} and num2 ${num2} is: ${num1+num2}`);
+console.log(`the subtraction of num1  ${num1} and num2 ${num2} is: ${num1-num2}`)
+console.log(`the multiplication of num1  ${num1} and num2 ${num2} is: ${num1*num2}`)
+console.log(`the division of num1  ${num1} and num2 ${num2} is: ${num1/num2}`)
+console.log(`the modulo of num1  ${num1} and num2 ${num2} is: ${num1%num2}`) 
+
+let a=10;
+let b='string';
+let c=true;
+let d=null;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+
+function greetuser(name){
+    console.log(`name of user is:${name}`)
+}
+greetuser('joyline christy');
+
+let temp=78.2;
+if (temp>30){
+    console.log('the given temp is greater than 30')
+
+}
+else{
+    console.log('the given temp is lesser than 30')
+
+}
+
+for(i=1;i<=5;i++) {
+    console.log(i)
+}
+    
+*/
